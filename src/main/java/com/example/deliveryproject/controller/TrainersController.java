b1d99228ce0c54e02e11e95c5f1dd2cefdc12e57
Nurislam Abdykadyrov <abdykadyrovnurislam21@gmail.com>
2023-04-18 15:11:01 +0600

    add SwaggerConfig

--- src/main/java/com/example/deliveryproject/controller/TrainersController.java
+++ src/main/java/com/example/deliveryproject/controller/TrainersController.java
@@ -1,15 +1,18 @@
 package com.example.deliveryproject.controller;
 
+import com.example.deliveryproject.dto.TrainersDto;
 import com.example.deliveryproject.entity.Trainers;
 import com.example.deliveryproject.service.TrainersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class TrainersController {
     private final TrainersService trainersService;
     @Autowired
@@ -20,7 +23,8 @@ public class TrainersController {
     @GetMapping("/trainers")
     public String getTrainers(Model model){
         List<Trainers> trainers = trainersService.getAllTrainers();
-        model.addAttribute("allTrainers",trainers);
-        return "index"; // Here, you are returning the name of the template file with the model object
+        List<TrainersDto> trainersDtos = trainers.stream().map(TrainersDto::from).collect(Collectors.toList());
+        model.addAttribute("allTrainers",trainersDtos);
+        return "index";
     }
 }
---
+++ src/main/java/com/example/deliveryproject/dto/TrainersDto.java
@@ -0,0 +1,28 @@
+package com.example.deliveryproject.dto;
+
+import com.example.deliveryproject.entity.Trainers;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TrainersDto {
+    public static TrainersDto from(Trainers trainers){
+        return builder()
+                .id(trainers.getId())
+                .title(trainers.getTitle())
+                .image(trainers.getImage())
+                .price(trainers.getPrice())
+                .gender(trainers.getGender())
+                .description(trainers.getDescription())
+                .build();
+    }
+
+    private Long id;
+    private String title;
+    private String image;
+    private double price;
+    private String gender;
+    private String description;
+
+}
