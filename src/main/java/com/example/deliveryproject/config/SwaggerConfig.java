1775bd38e7ae3a75abe06b7a63c6f278b48dd6d5
Nurislam Abdykadyrov <abdykadyrovnurislam21@gmail.com>
2023-04-20 19:34:47 +0600

    fixed SwaggerConfig

--- src/main/java/com/example/deliveryproject/config/SwaggerConfig.java
+++ src/main/java/com/example/deliveryproject/config/SwaggerConfig.java
@@ -1,35 +1,24 @@
-//package com.example.deliveryproject.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.todo.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("TODO List API")
-//                .description("REST API for managing personal to-do list.")
-//                .version("1.0.0")
-//                .build();
-//    }
-//
-//}
+package com.example.deliveryproject.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Spring Data REST API")
+                                .version("1.0.1")
+                );
+    }
+
+
+
+}
